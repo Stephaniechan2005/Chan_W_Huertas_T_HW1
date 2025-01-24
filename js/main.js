658c1adf684a08d1a31d7dfc5d868bb9f786be27
Stephaniechan2005 <stephanie_chan2005@yahoo.com.hk>
2025-01-23 21:08:20 -0500

    Add API
    The star war api added for character and movie

--- js/main.js
+++ js/main.js
@@ -2,23 +2,24 @@
   const charBox = document.querySelector("#char-box");
   const movieTemplate = document.querySelector("#movie-template");
   const movieCon = document.querySelector("#movie-con");
-  const baseUrl = "https://search.imdbot.workers.dev/";
+  const baseUrl = "https://swapi.dev/api/";
   const loader = document.querySelector("#loader");
   const loader2 = document.querySelector("#loader2");
 
   function getChar() {
     loader.classList.toggle("hidden");
-    fetch(`${baseUrl}?q=terminator`)
+    fetch(`${baseUrl}people`)
       .then((response) => response.json())
       .then(function (response) {
         console.log(response);
-        const chars = response.description;
+        const chars = response.results;
         const ul = document.createElement("ul");
         chars.forEach((char) => {
           const li = document.createElement("li");
           const a = document.createElement("a");
-          a.textContent = char["#TITLE"];
-          a.dataset.movie = char["#IMDB_ID"];
+          a.textContent = char["name"];
+          a.dataset.movie = char["films"];
+          // console.log(a.dataset.movie[1]);
           li.appendChild(a);
           ul.appendChild(li);
         });
@@ -38,26 +39,33 @@
       });
 
     function getMovie(e) {
-      loader2.classList.toggle("hidden");
       //console.log("getmovieed called"
       //console.log(e.currentTarget.dataset.movie);
-      const movieID = e.currentTarget.dataset.movie;
-      fetch(`${baseUrl}?tt=${movieID}`)
-        .then((response) => response.json())
-        .then(function (response) {
-          movieCon.innerHTML = "";
-          console.log(response.short.review.reviewBody);
-          const clone = movieTemplate.content.cloneNode(true);
-          const movieDescription = clone.querySelector(".movie-description");
-          movieDescription.innerHTML = response.short.review.reviewBody;
-          const movieHeading = clone.querySelector(".movie-heading");
-          movieHeading.innerHTML = response.short.name;
-          movieCon.appendChild(clone);
-          loader2.classList.toggle("hidden");
-        })
-        .catch(function (err) {
-          movieCon.innerHTML = "<p>No movie avaliable for this section</p>";
-        });
+      const movieIDs = e.currentTarget.dataset.movie.split(",");
+      movieIDs.forEach((movieID) => {
+        loader2.classList.toggle("hidden");
+        movieCon.innerHTML = "";
+        console.log(movieID);
+        fetch(`${movieID}`)
+          .then((response) => response.json())
+          .then(function (response) {
+            console.log(response.title);
+            const clone = movieTemplate.content.cloneNode(true);
+            const movieDescription = clone.querySelector(".movie-description");
+            movieDescription.innerHTML = response.opening_crawl;
+            const movieHeading = clone.querySelector(".movie-heading");
+            movieHeading.innerHTML = response.title;
+            const movieEp = clone.querySelector(".movie-ep");
+            movieEp.innerHTML = `Episode: ${response.episode_id}`;
+            const movieDirector = clone.querySelector(".movie-director");
+            movieDirector.innerHTML = `Director: ${response.director}`;
+            movieCon.appendChild(clone);
+            loader2.classList.toggle("hidden");
+          })
+          .catch(function (err) {
+            movieCon.innerHTML = "<p>No movie avaliable for this section</p>";
+          });
+      });
     }
   }
   getChar();
